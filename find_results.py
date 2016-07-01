c0c782f565e7a71939a58d416e3c1aa8a5807ebe
Allen Bargi <allen.bargi@gmail.com>
2016-07-02 00:21:11 +0200

    open multiple files with multiple cursor

--- find_results.py
+++ find_results.py
@@ -5,34 +5,33 @@ import re, os, shutil
 class FindInFilesOpenFileCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
-        line_no = self.get_line_no()
-        file_name = self.get_file()
-        if line_no is not None and file_name is not None:
-            file_loc = "%s:%s" % (file_name, line_no)
-            view.window().open_file(file_loc, sublime.ENCODED_POSITION)
-        elif file_name is not None:
-            view.window().open_file(file_name)
+        for sel in view.sel():
+            line_no = self.get_line_no(sel)
+            file_name = self.get_file(sel)
+            if line_no and file_name:
+                file_loc = "%s:%s" % (file_name, line_no)
+                view.window().open_file(file_loc, sublime.ENCODED_POSITION)
+            elif file_name:
+                view.window().open_file(file_name)
 
-    def get_line_no(self):
+    def get_line_no(self, sel):
         view = self.view
-        if len(view.sel()) == 1:
-            line_text = view.substr(view.line(view.sel()[0]))
-            match = re.match(r"\s*(\d+).+", line_text)
-            if match:
-                return match.group(1)
+        line_text = view.substr(view.line(sel))
+        match = re.match(r"\s*(\d+).+", line_text)
+        if match:
+            return match.group(1)
         return None
 
-    def get_file(self):
+    def get_file(self, sel):
         view = self.view
-        if len(view.sel()) == 1:
-            line = view.line(view.sel()[0])
-            while line.begin() > 0:
-                line_text = view.substr(line)
-                match = re.match(r"(.+):$", line_text)
-                if match:
-                    if os.path.exists(match.group(1)):
-                        return match.group(1)
-                line = view.line(line.begin() - 1)
+        line = view.line(sel)
+        while line.begin() > 0:
+            line_text = view.substr(line)
+            match = re.match(r"(.+):$", line_text)
+            if match:
+                if os.path.exists(match.group(1)):
+                    return match.group(1)
+            line = view.line(line.begin() - 1)
         return None
 
 
